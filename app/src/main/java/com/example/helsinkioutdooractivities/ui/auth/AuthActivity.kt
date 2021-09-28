6d82cfaf16473bb826c41a7003b586799f26be22
Katja Seikkinen <katjasei@metropolia.fi>
2021-09-28 22:59:24 +0300

    add logic code to controllers

--- app/src/main/java/com/example/helsinkioutdooractivities/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/helsinkioutdooractivities/ui/auth/AuthActivity.kt
@@ -1,4 +1,40 @@
 package com.example.helsinkioutdooractivities.ui.auth
 
-class AuthActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.helsinkioutdooractivities.R
+
+class AuthActivity:  AppCompatActivity(), FirstFragment.FirstFragmentListener {
+
+    //Create a new Fragment to be placed in the activity layout
+    private val firstFragment = FirstFragment()
+    private val loginFragment = LoginFragment()
+    private val registrationFragment = RegistrationFragment()
+
+    //FUNCTIONS:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+    }
+
+    //FirstFragment listeners:
+    //when button "sign up" clicked from FirstFragment
+    override fun onButtonSignUpClick() {
+        //replaceFragment(registrationFragment)
+    }
+    //when button "sign in" clicked from FirstFragment
+    override fun onButtonSignInClick() {
+        //replaceFragment(loginFragment)
+    }
+
+    //function used for fragment replacement
+    private fun replaceFragment(fragment: Fragment){
+        // fragment manager can help when switching to the other fragment is needed
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
 }
--- app/src/main/java/com/example/helsinkioutdooractivities/ui/auth/FirstFragment.kt
+++ app/src/main/java/com/example/helsinkioutdooractivities/ui/auth/FirstFragment.kt
@@ -1,4 +1,48 @@
 package com.example.helsinkioutdooractivities.ui.auth
 
-class FirstFragment {
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.helsinkioutdooractivities.R
+
+class FirstFragment: Fragment()  {
+
+    //VARIABLES
+    private var activityCallBack: FirstFragmentListener? = null
+
+    //FUNCTIONS AND INTERFACES
+    interface FirstFragmentListener {
+        fun onButtonSignUpClick()
+        fun onButtonSignInClick()
+    }
+
+    override fun onAttach(context: Context)   {
+        super.onAttach(context)
+        activityCallBack =  context as FirstFragmentListener
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        val buttonSignUp = view.findViewById<Button>(R.id.btn_sign_up)
+        val buttonSignIn = view.findViewById<Button>(R.id.btn_sign_in)
+
+        //callbacks for buttons in this fragment
+        //button Sign Up
+        buttonSignUp.setOnClickListener {
+            activityCallBack!!.onButtonSignUpClick()
+        }
+        //button Sign In
+        buttonSignIn.setOnClickListener {
+            activityCallBack!!.onButtonSignInClick()
+        }
+
+        return view
+    }
+
+
 }
