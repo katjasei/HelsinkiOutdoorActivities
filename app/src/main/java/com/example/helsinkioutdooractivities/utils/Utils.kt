e8258dd73daa998e508357ee0bdd7bc183910821
silja <silja@DESKTOP-SJHSFIU>
2021-12-02 22:25:32 +0200

    scrollview to gyms overview

--- app/src/main/java/com/example/helsinkioutdooractivities/utils/Utils.kt
+++ app/src/main/java/com/example/helsinkioutdooractivities/utils/Utils.kt
@@ -14,7 +14,7 @@ fun hideSystemUI(window: Window) {
             or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             or View.SYSTEM_UI_FLAG_FULLSCREEN)
-       /* if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.setDecorFitsSystemWindows(false)
             window.insetsController?.let {
                 it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
@@ -28,7 +28,7 @@ fun hideSystemUI(window: Window) {
                     or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
-        }*/
+        }
     }
 
 //function used for fragment replacement
